0a2fef916df09378608a4552ba360fcd38e05331
Andrew Van <vanandrew@wustl.edu>
2020-12-06 03:32:14 -0600

    update 2

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import cProfile
 
 img = nib.load("data/T1_test.nii.gz")
 bet = BrainExtractor(img)
-test = cProfile.run("bet.run(50)", sort="percall")
+test = cProfile.run("bet.run(1000)")
 mask = bet.compute_mask()
 sbv.plot_brain(mask)
 breakpoint()
